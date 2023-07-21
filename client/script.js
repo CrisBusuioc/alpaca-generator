8409c731a39ec574a647340695a3158b426c0001
CrisBusuioc <mr.bucuresti2@gmail.com>
2023-07-21 10:48:56 +0100

    Background picture now displayed

--- client/script.js
+++ client/script.js
@@ -1,12 +1,16 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.4.0/+esm';
 
+let dataReceived;
 
 // Script function
 function randomBackground() {
   // Client-side code
-axios.get('http://localhost:3000/get-images')
-.then(res => console.log(res.data))
-}
+  axios.get('http://localhost:3000/get-images')
+  .then(res => {
+  dataReceived = res.data.images
 
+  document.getElementById('image').src = 'alpaca-generator-parts/alpaca/backgrounds' + `/${dataReceived[Math.floor(Math.random() * 18)]}`
+})
+}
 // Usage example:
-console.log(randomBackground())
+randomBackground()
